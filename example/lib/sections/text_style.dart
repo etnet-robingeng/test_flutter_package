517309fa65ae3483b272ebdc39bb5e9cb0e60880
mitchell6543 <mitchelllam@etnet.com.hk>
2025-03-20 16:38:21 +0800

    update custom fonts

--- example/lib/sections/text_style.dart
+++ example/lib/sections/text_style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:single_child_two_dimensional_scroll_view/single_child_two_dimensional_scroll_view.dart';
 import 'package:app_family_materials/family_textStyles.dart';
 
 class TextStylePage extends StatelessWidget {
@@ -6,61 +7,150 @@ class TextStylePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Text Styles'),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildTwoDimensionalScrollView(
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Headline Styles:', style: FamilyTextStyle.LHeadlineSemiBold),
-              const SizedBox(height: 16),
-              Text('Small Headline Regular', style: FamilyTextStyle.SHeadlineRegular),
-              const SizedBox(height: 8),
-              Text('Small Headline Medium', style: FamilyTextStyle.SHeadlineMedium),
-              const SizedBox(height: 8),
-              Text('Small Headline SemiBold', style: FamilyTextStyle.SHeadlineSemiBold),
-              const SizedBox(height: 8),
-              Text('Medium Headline Regular', style: FamilyTextStyle.MHeadlineRegular),
-              const SizedBox(height: 8),
-              Text('Medium Headline Medium', style: FamilyTextStyle.MHeadlineMedium),
-              const SizedBox(height: 8),
-              Text('Medium Headline SemiBold', style: FamilyTextStyle.MHeadlineSemiBold),
-              const SizedBox(height: 8),
-              Text('Large Headline Regular', style: FamilyTextStyle.LHeadlineRegular),
-              const SizedBox(height: 8),
-              Text('Large Headline Medium', style: FamilyTextStyle.LHeadlineMedium),
-              const SizedBox(height: 8),
-              Text('Large Headline SemiBold', style: FamilyTextStyle.LHeadlineSemiBold),
-              const SizedBox(height: 24),
-              Text('Body Styles:', style: FamilyTextStyle.LHeadlineSemiBold),
-              const SizedBox(height: 16),
-              Text('Small Body Regular', style: FamilyTextStyle.SBodyRegular),
-              const SizedBox(height: 8),
-              Text('Small Body Medium', style: FamilyTextStyle.SBodyMedium),
-              const SizedBox(height: 8),
-              Text('Small Body SemiBold', style: FamilyTextStyle.SBodySemiBold),
-              const SizedBox(height: 8),
-              Text('Medium Body Regular', style: FamilyTextStyle.MBodyRegular),
-              const SizedBox(height: 8),
-              Text('Medium Body Medium', style: FamilyTextStyle.MBodyMedium), 
-              const SizedBox(height: 8),
-              Text('Medium Body SemiBold', style: FamilyTextStyle.MBodySemiBold),
-              const SizedBox(height: 8),
-              Text('Large Body Regular', style: FamilyTextStyle.LBodyRegular),
-              const SizedBox(height: 8),
-              Text('Large Body Medium', style: FamilyTextStyle.LBodyMedium),
-              const SizedBox(height: 8),
-              Text('Large Body SemiBold', style: FamilyTextStyle.LBodySemiBold),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildHeadlineStyles(),
+                  const SizedBox(height: 16),
+                  _buildBodyStyles(),
+                ],
+              ),
+              const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildHeadlineStylesEN(),
+                  const SizedBox(height: 16),
+                  _buildBodyStylesEN(),
+                ],
+              )
             ],
-          ),
+          )
         ),
       ),
+    );
+  }
+
+  Widget _buildHeadlineStyles() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(FamilyTextStyle.fontFamily, style: FamilyTextStyle.LHeadlineSemiBold),
+        const SizedBox(height: 16),
+        Text('標題樣式:', style: FamilyTextStyle.MHeadlineSemiBold),
+        const SizedBox(height: 16),
+        Text('小號標題常規', style: FamilyTextStyle.SHeadlineRegular),
+        const SizedBox(height: 8),
+        Text('小號標題中等', style: FamilyTextStyle.SHeadlineMedium),
+        const SizedBox(height: 8),
+        Text('小號標題半粗體', style: FamilyTextStyle.SHeadlineSemiBold),
+        const SizedBox(height: 8),
+        Text('中號標題常規', style: FamilyTextStyle.MHeadlineRegular),
+        const SizedBox(height: 8),
+        Text('中號標題中等', style: FamilyTextStyle.MHeadlineMedium),
+        const SizedBox(height: 8),
+        Text('中號標題半粗體', style: FamilyTextStyle.MHeadlineSemiBold),
+        const SizedBox(height: 8),
+        Text('大號標題常規', style: FamilyTextStyle.LHeadlineRegular),
+        const SizedBox(height: 8),
+        Text('大號標題中等', style: FamilyTextStyle.LHeadlineMedium),
+        const SizedBox(height: 8),
+        Text('大號標題半粗體', style: FamilyTextStyle.LHeadlineSemiBold),
+      ],
+    );
+  }
+
+  Widget _buildBodyStyles() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('內文樣式:', style: FamilyTextStyle.LHeadlineSemiBold),
+        const SizedBox(height: 16),
+        Text('小號內文常規', style: FamilyTextStyle.SBodyRegular),
+        const SizedBox(height: 8),
+        Text('小號內文中等', style: FamilyTextStyle.SBodyMedium),
+        const SizedBox(height: 8),
+        Text('小號內文半粗體', style: FamilyTextStyle.SBodySemiBold),
+        const SizedBox(height: 8),
+        Text('中號內文常規', style: FamilyTextStyle.MBodyRegular),
+        const SizedBox(height: 8),
+        Text('中號內文中等', style: FamilyTextStyle.MBodyMedium),
+        const SizedBox(height: 8),
+        Text('中號內文半粗體', style: FamilyTextStyle.MBodySemiBold),
+        const SizedBox(height: 8),
+        Text('大號內文常規', style: FamilyTextStyle.LBodyRegular),
+        const SizedBox(height: 8),
+        Text('大號內文中等', style: FamilyTextStyle.LBodyMedium),
+        const SizedBox(height: 8),
+        Text('大號內文半粗體', style: FamilyTextStyle.LBodySemiBold),
+      ],
+    );
+  }
+
+  Widget _buildHeadlineStylesEN() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(FamilyTextStyleEN.fontFamily, style: FamilyTextStyle.LHeadlineSemiBold),
+        const SizedBox(height: 16),
+        Text('Headline Styles:', style: FamilyTextStyleEN.LHeadlineSemiBold),
+        const SizedBox(height: 16),
+        Text('Small Headline Regular 1234567890', style: FamilyTextStyleEN.SHeadlineRegular),
+        const SizedBox(height: 8),
+        Text('Small Headline Medium 1234567890', style: FamilyTextStyleEN.SHeadlineMedium),
+        const SizedBox(height: 8),
+        Text('Small Headline SemiBold 1234567890', style: FamilyTextStyleEN.SHeadlineSemiBold),
+        const SizedBox(height: 8),
+        Text('Medium Headline Regular 1234567890', style: FamilyTextStyleEN.MHeadlineRegular),
+        const SizedBox(height: 8),
+        Text('Medium Headline Medium 1234567890', style: FamilyTextStyleEN.MHeadlineMedium),
+        const SizedBox(height: 8),
+        Text('Medium Headline SemiBold 1234567890', style: FamilyTextStyleEN.MHeadlineSemiBold),
+        const SizedBox(height: 8),
+        Text('Large Headline Regular 1234567890', style: FamilyTextStyleEN.LHeadlineRegular),
+        const SizedBox(height: 8),
+        Text('Large Headline Medium 1234567890', style: FamilyTextStyleEN.LHeadlineMedium),
+        const SizedBox(height: 8),
+        Text('Large Headline SemiBold 1234567890', style: FamilyTextStyleEN.LHeadlineSemiBold),
+      ],
+    );
+  }
+
+  Widget _buildBodyStylesEN() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Body Styles:', style: FamilyTextStyle.LHeadlineSemiBold),
+        const SizedBox(height: 16),
+        Text('Small Body Regular 1234567890', style: FamilyTextStyleEN.SBodyRegular),
+        const SizedBox(height: 8),
+        Text('Small Body Medium 1234567890', style: FamilyTextStyleEN.SBodyMedium),
+        const SizedBox(height: 8),
+        Text('Small Body SemiBold 1234567890', style: FamilyTextStyleEN.SBodySemiBold),
+        const SizedBox(height: 8),
+        Text('Medium Body Regular 1234567890', style: FamilyTextStyleEN.MBodyRegular),
+        const SizedBox(height: 8),
+        Text('Medium Body Medium 1234567890', style: FamilyTextStyleEN.MBodyMedium),
+        const SizedBox(height: 8),
+        Text('Medium Body SemiBold 1234567890', style: FamilyTextStyleEN.MBodySemiBold),
+        const SizedBox(height: 8),
+        Text('Large Body Regular 1234567890', style: FamilyTextStyleEN.LBodyRegular),
+        const SizedBox(height: 8),
+        Text('Large Body Medium 1234567890', style: FamilyTextStyleEN.LBodyMedium),
+        const SizedBox(height: 8),
+        Text('Large Body SemiBold 1234567890', style: FamilyTextStyleEN.LBodySemiBold),
+      ],
     );
   }
 }
--- lib/family_textStyles.dart
+++ lib/family_textStyles.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart' show TextStyle, FontWeight;
 import 'family_sizes.dart';
 
 class FamilyTextStyle {
-  static const fontFamily = 'PingFang TC';
+  static const fontFamily = 'PingFangTC';
 
   static const TextStyle SBodyRegular = TextStyle(
     fontSize: FamilySizesBody.fontSizeS,
@@ -131,4 +131,134 @@ class FamilyTextStyle {
     height: FamilySizeHeadlineHeight.fontHeightL,
     fontFamily: fontFamily,
   );
+}
+
+class FamilyTextStyleEN extends FamilyTextStyle {
+  static const String fontFamily = 'HarmonyOS_Sans';
+
+  static const TextStyle SBodyRegular = TextStyle(
+    fontSize: FamilySizesBody.fontSizeS,
+    fontWeight: FontWeight.w400,
+    height: FamilySizeBodyHeight.fontHeightS,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle SBodyMedium = TextStyle(
+    fontSize: FamilySizesBody.fontSizeS,
+    fontWeight: FontWeight.w500,
+    height: FamilySizeBodyHeight.fontHeightS,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle SBodySemiBold = TextStyle(
+    fontSize: FamilySizesBody.fontSizeS,
+    fontWeight: FontWeight.w600,
+    height: FamilySizeBodyHeight.fontHeightS,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle MBodyRegular = TextStyle(
+    fontSize: FamilySizesBody.fontSizeM,
+    fontWeight: FontWeight.w400,
+    height: FamilySizeBodyHeight.fontHeightM,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle MBodyMedium = TextStyle(
+    fontSize: FamilySizesBody.fontSizeM,
+    fontWeight: FontWeight.w500,
+    height: FamilySizeBodyHeight.fontHeightM,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle MBodySemiBold = TextStyle(
+    fontSize: FamilySizesBody.fontSizeM,
+    fontWeight: FontWeight.w600,
+    height: FamilySizeBodyHeight.fontHeightM,
+    fontFamily: fontFamily, 
+  );
+
+  static const TextStyle LBodyRegular = TextStyle(
+    fontSize: FamilySizesBody.fontSizeL,
+    fontWeight: FontWeight.w400,
+    height: FamilySizeBodyHeight.fontHeightL,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle LBodyMedium = TextStyle(
+    fontSize: FamilySizesBody.fontSizeL,
+    fontWeight: FontWeight.w500,
+    height: FamilySizeBodyHeight.fontHeightL,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle LBodySemiBold = TextStyle(
+    fontSize: FamilySizesBody.fontSizeL,
+    fontWeight: FontWeight.w600,
+    height: FamilySizeBodyHeight.fontHeightL,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle SHeadlineRegular = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeS,
+    fontWeight: FontWeight.w400,
+    height: FamilySizeHeadlineHeight.fontHeightS,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle SHeadlineMedium = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeS,
+    fontWeight: FontWeight.w500,
+    height: FamilySizeHeadlineHeight.fontHeightS,
+    fontFamily: fontFamily,
+  );
+  
+  static const TextStyle SHeadlineSemiBold = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeS,
+    fontWeight: FontWeight.w600,
+    height: FamilySizeHeadlineHeight.fontHeightS,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle MHeadlineRegular = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeM,
+    fontWeight: FontWeight.w400,
+    height: FamilySizeHeadlineHeight.fontHeightM,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle MHeadlineMedium = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeM,
+    fontWeight: FontWeight.w500,
+    height: FamilySizeHeadlineHeight.fontHeightM,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle MHeadlineSemiBold = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeM,
+    fontWeight: FontWeight.w600,
+    height: FamilySizeHeadlineHeight.fontHeightM,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle LHeadlineRegular = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeL,
+    fontWeight: FontWeight.w400,
+    height: FamilySizeHeadlineHeight.fontHeightL,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle LHeadlineMedium = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeL,
+    fontWeight: FontWeight.w500,
+    height: FamilySizeHeadlineHeight.fontHeightL,
+    fontFamily: fontFamily,
+  );
+
+  static const TextStyle LHeadlineSemiBold = TextStyle(
+    fontSize: FamilySizesHeadline.fontSizeL,
+    fontWeight: FontWeight.w600,
+    height: FamilySizeHeadlineHeight.fontHeightL,
+    fontFamily: fontFamily,
+  );  
 }
